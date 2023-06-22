8e759de13d964a3091a35d42d0a6dcc95e18cf35
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-06-22 12:56:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Dagpenger-SERVICENAME
+
+## Komme i gang
+
+Gradle brukes som byggverktøy og er bundlet inn.
+
+`./gradlew build`
+
+## Sjekkliste for ny app
+1. Bytt navn på app i nais.yaml, settings.gradle og dockerfile.
+2. Fjern kommentering i deploy.yaml (står beskrevet hva man skal kommentere ut)
+3. Legge til deploysecreten i repoet.
+
+---
+
+# Henvendelser
+
+Spørsmål knyttet til koden eller prosjektet kan rettes mot:
+
+* André Roaldseth, andre.roaldseth@nav.no
+* Eller en annen måte for omverden å kontakte teamet på
+
+## For NAV-ansatte
+
+Interne henvendelser kan sendes via Slack i kanalen #dagpenger.
---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    kotlin("jvm") version Kotlin.version
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+application {
+    mainClass.set("no.nav.dagpenger.SERVICENAME")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+dependencies {
+    implementation(RapidAndRivers)
+
+    implementation(Konfig.konfig)
+    implementation(Kotlin.Logging.kotlinLogging)
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        showExceptions = true
+        showStackTraces = true
+        exceptionFormat = TestExceptionFormat.FULL
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    }
+}
+
+tasks.withType<Jar>().configureEach {
+    dependsOn("test")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes(mapOf("Main-Class" to application.mainClass.get()))
+    }
+
+    from(
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    )
+}
