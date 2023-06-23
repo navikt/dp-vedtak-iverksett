8350386528d94827721f0a2bcaf44db46ce85ae3
Marius Eriksen <marius.eriksen@nav.no>
2023-06-23 08:50:34 +0200

    Legg til testcaser i forberedelse
    Co-authored-by: Abdifatah Bashi <abdifatah.bashi@nav.no>

---
+++ src/test/kotlin/no/nav/dagpenger/vedtak/iverksett/VedtakIverksettingTest.kt
@@ -0,0 +1,22 @@
+package no.nav.dagpenger.vedtak.iverksett
+
+import org.junit.jupiter.api.Test
+
+class VedtakIverksettingTest {
+
+    @Test
+    fun `motta iverksettingsbehov for rammevedtak, kaller iverksett og løser behovet`() {
+    }
+
+    @Test
+    fun `motta iverksettingsbehov for løpendeVedtak, kaller iverksett og løs behovet`() {
+    }
+
+    @Test
+    fun `motta iverksettingsbehov for rammevedtak, iverksett feiler`() {
+    }
+
+    @Test
+    fun `motta iverksettingsbehov for løpendeVedtak, iverksett feiler`() {
+    }
+}
