135c03af1db9b72dfa3aec910baebccf0b1af85c
Marius Eriksen <marius.eriksen@nav.no>
2023-09-18 13:26:28 +0200

    Fjern kode for kjeding av behandlinger, i forberedelse til å flytte iverksettingsmodellen over i denne appen.
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- src/test/kotlin/no/nav/dagpenger/vedtak/iverksett/IverksettUtbetalingBehovløserTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtak/iverksett/IverksettUtbetalingBehovløserTest.kt
@@ -1,7 +1,6 @@
 package no.nav.dagpenger.vedtak.iverksett
 
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -22,7 +21,7 @@ class IverksettUtbetalingBehovløserTest {
     }
 
     @Test
-    fun `motta iverksettingsbehov for utbetalingsvedtak uten forrigeBehandlingId, kaller iverksett og løs behovet`() {
+    fun `motta iverksettingsbehov for utbetalingsvedtak, kaller iverksett og løser behovet`() {
         val iverksettDtoSlot = slot<IverksettDto>()
         coEvery { iverksettClient.iverksett(capture(iverksettDtoSlot)) } just Runs
 
@@ -37,25 +36,5 @@ class IverksettUtbetalingBehovløserTest {
         val iverksattUtbetalingsvedtak = testRapid.inspektør.message(0)
         iverksattUtbetalingsvedtak["@løsning"]["IverksettUtbetaling"].asBoolean() shouldBe true
         iverksattUtbetalingsvedtak["IverksettUtbetaling"]["utbetalingsdager"].size() shouldBe 10
-        iverksattUtbetalingsvedtak["IverksettUtbetaling"]["forrigeBehandlingId"] shouldBe null
-    }
-
-    @Test
-    fun `motta iverksettingsbehov for utbetalingsvedtak med forrigeBehandlingId, kaller iverksett og løs behovet`() {
-        val iverksettDtoSlot = slot<IverksettDto>()
-        coEvery { iverksettClient.iverksett(capture(iverksettDtoSlot)) } just Runs
-
-        testRapid.sendTestMessage(behovOmIverksettingAvUtbetalingsvedtakMedForrigeBehandlingId())
-
-        coVerify(exactly = 1) {
-            iverksettClient.iverksett(any())
-        }
-        iverksettDtoSlot.isCaptured shouldBe true
-
-        testRapid.inspektør.size shouldBe 1
-        val iverksattUtbetalingsvedtak = testRapid.inspektør.message(0)
-        iverksattUtbetalingsvedtak["@løsning"]["IverksettUtbetaling"].asBoolean() shouldBe true
-        iverksattUtbetalingsvedtak["IverksettUtbetaling"]["utbetalingsdager"].size() shouldBe 10
-        iverksattUtbetalingsvedtak["IverksettUtbetaling"]["forrigeBehandlingId"] shouldNotBe null
     }
 }
