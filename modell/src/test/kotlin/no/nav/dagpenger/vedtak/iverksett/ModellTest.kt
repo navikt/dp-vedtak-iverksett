2c3bab3acbeb64f9860f9bfafe6a41f69211e9c8
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2023-09-26 15:33:38 +0200

    Refaktorert test. Sjekker på to utbetalingsvedtak.
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- modell/src/test/kotlin/no/nav/dagpenger/vedtak/iverksett/ModellTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/vedtak/iverksett/ModellTest.kt
@@ -14,59 +14,73 @@ import java.util.UUID
 class ModellTest {
 
     private val ident = "12345678911".tilPersonIdentfikator()
-    private val vedtakId = UUID.randomUUID()
-    private val behandlingId = UUID.randomUUID()
+    private val vedtakId1 = UUID.randomUUID()
+    private val vedtakId2 = UUID.randomUUID()
+    private val behandlingId1 = UUID.randomUUID()
+    private val behandlingId2 = UUID.randomUUID()
     private val sakId = SakId("SAKSNUMMER_1")
-    private val vedtakstidspunkt = LocalDateTime.now()
     private val ukedagIdag = LocalDate.now().dayOfWeek
-    private val virkningsdatoErSøndag: LocalDate = LocalDate.now().minusDays(ukedagIdag.value.toLong())
+    private val virkningsdato1: LocalDate = LocalDate.now().minusDays(ukedagIdag.value.toLong())
+    private val virkningsdato2: LocalDate = virkningsdato1.plusDays(14)
     private val modellInspektør get() = ModellInspektør(sak)
     private val sak = Sak(ident = ident, sakId = sakId, iverksettinger = mutableListOf())
 
     @Test
     fun `Utbetalingsvedtak fører til oppdatert iverksettingsmodell`() {
-        sak.håndter(utbetalingsvedtakFattetHendelse(Aktivitetslogg()))
+        sak.håndter(utbetalingsvedtakFattetHendelse(vedtakId1, behandlingId1, virkningsdato1, Aktivitetslogg()))
 
         assertSoftly {
             modellInspektør.sakId shouldBe sakId
             modellInspektør.ident shouldBe ident
-            modellInspektør.vedtakId shouldBe vedtakId
-            modellInspektør.virkningsdato shouldBe virkningsdatoErSøndag
+            modellInspektør.vedtakId shouldBe vedtakId1
+            modellInspektør.virkningsdato shouldBe virkningsdato1
             modellInspektør.iverksettingsdager.size shouldBe 14
-            utbetalingsdager().forEach { utbetalingsdag ->
+            utbetalingsdager(virkningsdato1).forEach { utbetalingsdag ->
+                modellInspektør.iverksettingsdager[utbetalingsdag.dato]!!.verdi shouldBe utbetalingsdag.beløp
+            }
+        }
+        sak.håndter(utbetalingsvedtakFattetHendelse(vedtakId2, behandlingId2, virkningsdato2, Aktivitetslogg()))
+
+        assertSoftly {
+            modellInspektør.sakId shouldBe sakId
+            modellInspektør.ident shouldBe ident
+            modellInspektør.vedtakId shouldBe vedtakId2
+            modellInspektør.virkningsdato shouldBe virkningsdato2
+            modellInspektør.iverksettingsdager.size shouldBe 28
+            utbetalingsdager(virkningsdato2).forEach { utbetalingsdag ->
                 modellInspektør.iverksettingsdager[utbetalingsdag.dato]!!.verdi shouldBe utbetalingsdag.beløp
             }
         }
     }
 
-    private fun utbetalingsvedtakFattetHendelse(aktivitetslogg: Aktivitetslogg) =
+    private fun utbetalingsvedtakFattetHendelse(vedtakId: UUID, behandlingId: UUID, virkningsdato: LocalDate, aktivitetslogg: Aktivitetslogg) =
         UtbetalingsvedtakFattetHendelse(
             meldingsreferanseId = UUID.randomUUID(),
             ident = ident.identifikator(),
             vedtakId = vedtakId,
             behandlingId = behandlingId,
             sakId = sakId.sakId,
-            vedtakstidspunkt = vedtakstidspunkt,
-            virkningsdato = virkningsdatoErSøndag,
-            utbetalingsdager = utbetalingsdager(),
+            vedtakstidspunkt = LocalDateTime.now(),
+            virkningsdato = virkningsdato,
+            utbetalingsdager = utbetalingsdager(virkningsdato),
             utfall = UtbetalingsvedtakFattetHendelse.Utfall.Innvilget,
             aktivitetslogg = aktivitetslogg,
         )
 
-    private fun utbetalingsdager() = listOf(
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(13), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(12), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(11), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(10), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(9), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(8), beløp = 0.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(7), beløp = 0.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(6), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(5), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(4), beløp = 500.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(3), beløp = 250.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(2), beløp = 300.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag.minusDays(1), beløp = 0.0),
-        Utbetalingsdag(dato = virkningsdatoErSøndag, beløp = 0.0),
+    private fun utbetalingsdager(virkningsdato: LocalDate) = listOf(
+        Utbetalingsdag(dato = virkningsdato.minusDays(13), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(12), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(11), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(10), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(9), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(8), beløp = 0.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(7), beløp = 0.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(6), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(5), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(4), beløp = 500.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(3), beløp = 250.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(2), beløp = 300.0),
+        Utbetalingsdag(dato = virkningsdato.minusDays(1), beløp = 0.0),
+        Utbetalingsdag(dato = virkningsdato, beløp = 0.0),
     )
 }
